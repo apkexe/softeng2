211743e2acc8ad39e49e3696510c03d8cd778d73
Themis <themistoklisarv2001@gmail.com>
2023-12-17 12:54:34 +0200

    test for user contract put requests

--- tests/init.test.js
+++ tests/init.test.js
@@ -3,7 +3,7 @@ const test = require('ava');
 const listen = require('test-listen');
 const got = require('got');
 
-const {} = require('../service/DefaultService.js');
+const {getPosts} = require('../service/DefaultService.js');
 const app = require('../index.js');
 
 test.before(async (t) => {
